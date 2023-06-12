0bdf1f42a9d43c23efb5084ddb9ffb14d04c12e9
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-12 00:45:18 -0400

    Add task2

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.Write("Enter a day of the day of the week: ");
+string weekday = Console.ReadLine();
+int dayNumber = Convert.ToInt32(weekday);
+
+if (dayNumber < 0 || dayNumber > 7)
+{
+    Console.WriteLine("Such day of a week does not exist!");
+}
+else
+{
+    Console.WriteLine(week[dayNumber -1]);
+}
