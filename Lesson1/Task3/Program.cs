c81bcf6e1e468e7dc21def318784b78e7207929e
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-12 11:27:38 -0400

    Add Task3

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a whole number");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = -number1;
+while (number2 <= number1)
+{
+    Console.Write($"{number2} ");
+    number2++;
+}
+
