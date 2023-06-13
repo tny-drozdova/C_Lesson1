19ced429093685912f94ef04f4610c98423c655f
Tanya Drozdova <tny.drozdova@gmail.com>
2023-06-12 21:18:48 -0400

    Add Task0

---
+++ Lesson1/Task0/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a number => ");  // Display the invitation on the terminal/screen
+string inputedString = Console.ReadLine(); // User's entry
+int number = Convert.ToInt32(inputedString); // The string becames a number
+// Console.WriteLine("We entered a number " + number + "!");
+Console.WriteLine($"We entered a number {number}!"); // Display of the entered munber on the screen/terminal
+
+int square = number * number;  // The formula for the square of the number
+Console.WriteLine($"Square of {number} equals {square}"); // Display of the result on the screen/terminal
+Console.WriteLine($"Thanks!");
